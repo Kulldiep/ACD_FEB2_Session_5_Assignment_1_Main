d7ab6d6bf74beaf00bc2fdcff233c92bc53f7b8c
Kulldiep <kulldiep@gmail.com>
2017-06-06 00:07:10 +0530

    Assgnment5.1

---
+++ app.js
@@ -0,0 +1,10 @@
+
+var name= 'Kulldiep';
+var age= 25;
+var dob= '10 July 1991';
+var place= 'Chennai';
+
+document.getElementById("myName").innerHTML = name;
+document.getElementById("myAge").innerHTML = age;
+document.getElementById("myBirthday").innerHTML = dob;
+document.getElementById("myPlace").innerHTML = place;
